4ee0069a208e9508264c954e0910f861877ca5c8
David Wickes <dave.wickes@gmail.com>
2014-11-25 16:50:56 +0000

    Refactors out game instantiation.

--- src/test/java/com/gypsydave5/bowling/BowlingGameTest.java
+++ src/test/java/com/gypsydave5/bowling/BowlingGameTest.java
@@ -1,21 +1,27 @@
 package com.gypsydave5.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class BowlingGameTest {
 
+    Game game;
+
+    @Before public void
+    setUpGame() {
+        game = new Game();
+    }
+
     @Test public void
     itScoresZeroOnGutterGames() {
-        Game game = new Game();
         for (int i = 0; i < 20; i++) game.roll(0);
         assertThat(game.score(), is(0));
     }
 
     @Test public void
     itScoresTwentyOnGamesOfAllOnes() {
-        Game game = new Game();
         for (int i = 0; i < 20; i++) game.roll(1);
         assertThat(game.score(), is(20));
     }
