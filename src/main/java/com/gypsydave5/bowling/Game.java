6d9a5896a10c1deef63714a88a4b76ea26071325
David Wickes <dave.wickes@gmail.com>
2014-11-25 22:11:49 +0000

    Removes default template.

--- src/main/java/com/gypsydave5/bowling/Game.java
+++ src/main/java/com/gypsydave5/bowling/Game.java
@@ -1,8 +1,5 @@
 package com.gypsydave5.bowling;
 
-/**
- * Created by gypsydave5 on 25/11/14.
- */
 public class Game {
 
     private int rolls[] = new int[21];
@@ -14,7 +11,39 @@ public class Game {
 
     public int score() {
         int score = 0;
-        for (int roll : rolls) score += roll;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)){
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
         return score;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex+2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return sumOfBallsInFrame(frameIndex) == 10;
     }
 }
--- src/test/java/com/gypsydave5/bowling/BowlingGameTest.java
+++ src/test/java/com/gypsydave5/bowling/BowlingGameTest.java
@@ -9,10 +9,6 @@ public class BowlingGameTest {
 
     Game game;
 
-    private void manyRolls(int iterations, int pins) {
-        for (int i = 0; i < iterations; i++) game.roll(pins);
-    }
-
     @Before public void
     setUpGame() {
         game = new Game();
@@ -20,24 +16,52 @@ public class BowlingGameTest {
 
     @Test public void
     itScoresZeroOnGutterGames() {
-        manyRolls(20, 0);
+        rollMany(20, 0);
         assertThat(game.score(), is(0));
     }
 
     @Test public void
     itScoresTwentyOnGamesOfAllOnes() {
-        manyRolls(20, 1);
+        rollMany(20, 1);
         assertThat(game.score(), is(20));
     }
 
-//    @Test public void
-//    itKnowsHowToScoreOneSpare() {
-//        game.roll(5);
-//        game.roll(5);
-//        game.roll(4);
-//        manyRolls(17 ,0);
-//        assertThat(game.score(), is(18));
-//    }
+    @Test public void
+    itKnowsHowToScoreOneSpare() {
+        rollSpare();
+        game.roll(4);
+        rollMany(17, 0);
+        assertThat(game.score(), is(18));
+    }
+
+    @Test public void
+    itKnowsHowToScoreAStrike() {
+        rollStrike();
+        game.roll(2);
+        game.roll(3);
+        rollMany(17, 0);
+        assertThat(game.score(), is(20));
+    }
+
+    @Test public void
+    itKnowsHowToScoreAPerfectGame() {
+        rollMany(12, 10);
+        assertThat(game.score(), is(300));
+    }
+
+    private void rollMany(int iterations, int pins) {
+        for (int i = 0; i < iterations; i++) game.roll(pins);
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
+
 
 }
 
