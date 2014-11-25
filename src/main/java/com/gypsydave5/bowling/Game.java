c2145cc9f90b5d1b71837daef01036a361638faf
David Wickes <dave.wickes@gmail.com>
2014-11-25 16:39:43 +0000

    Creates second test for a game of all 1s

--- src/main/java/com/gypsydave5/bowling/Game.java
+++ src/main/java/com/gypsydave5/bowling/Game.java
@@ -4,4 +4,12 @@ package com.gypsydave5.bowling;
  * Created by gypsydave5 on 25/11/14.
  */
 public class Game {
+
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
 }
---
+++ src/test/java/com/gypsydave5/bowling/BowlingGameTest.java
@@ -0,0 +1,27 @@
+package com.gypsydave5.bowling;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class BowlingGameTest {
+
+    @Test public void
+    itScoresZeroOnGutterGames() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertThat(game.score(), is(0));
+    }
+
+    @Test public void
+    itScoresTwentyOnGamesOfAllOnes() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertThat(game.score(), is(20));
+    }
+
+}
