47c6a2bacb3015e7225c32dc5f68ae17042f8818
David Wickes <dave.wickes@gmail.com>
2014-11-25 17:24:43 +0000

    Removes unused score field.

--- src/main/java/com/gypsydave5/bowling/Game.java
+++ src/main/java/com/gypsydave5/bowling/Game.java
@@ -5,13 +5,16 @@ package com.gypsydave5.bowling;
  */
 public class Game {
 
-    int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) score += rolls[i];
         return score;
     }
 }
